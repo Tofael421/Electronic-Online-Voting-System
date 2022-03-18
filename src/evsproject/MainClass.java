e8dcab53b410192d55922eebc7837f8af6dc7f2d
Tofael421 <tofaelahmed421@gmail.com>
2022-03-18 16:28:16 +0600

    100% done use java oop concept

---
+++ src/evsproject/MainClass.java
@@ -0,0 +1,28 @@
+package evsproject;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        
+       LoginOrVotePage login = new LoginOrVotePage();
+      login.setVisible(true);
+        //login.setLocationRelativeTo(null);
+        //login.setResizable(false);
+      //  AdminPage ad = new AdminPage();
+       //EmployeePage ep = new EmployeePage();
+        //ep.setVisible(true);
+        
+       // ad.setVisible(true);
+       // ad.setLocationRelativeTo(null);
+      // VoteSubmit vote = new VoteSubmit();
+       //vote.setVisible(true);
+       //vote.setLocationRelativeTo(null);
+      // Mark mark = new Mark();
+       //mark.setVisible(true);
+       //mark.setLocationRelativeTo(null);
+       //VoteSubmit vs = new VoteSubmit();
+       //vs.setVisible(true);
+       //vs.setLocationRelativeTo(null);
+    }
+    
+}
